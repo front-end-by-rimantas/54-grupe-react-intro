9cb289d851fed2bcbd9dd00e5b8e2b739f6433cb
Rimantas <belauzas@gmail.com>
2025-06-12 14:05:42 +0300

    Todo style

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -3,8 +3,18 @@ import style from './Todo.module.css';
 export function Todo() {
     return (
         <div className={style.todo}>
-            <p className={style}>CONTENT</p>
-            <button className={style.btn}>Click me</button>
+            <form className={style.form}>
+                <input className={style.input} type="text" />
+                <button className={style.btn} type='submit'>Create</button>
+            </form>
+            <ul className={style.ul}>
+                <li className={style.li}>
+                    <p className={style.p}>Text</p>
+                    <div className={style.actions}>
+                        <button className={style.btn}>Delete</button>
+                    </div>
+                </li>
+            </ul>
         </div>
     );
 }
